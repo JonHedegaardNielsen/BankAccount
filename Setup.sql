54b8e52ae7182406acb147475565c8c77ad8a7b9
jon <jon230706@gmail.com>
2024-08-21 07:37:45 +0200

    updated sql script

--- Setup.sql
+++ Setup.sql
@@ -1,10 +1,18 @@
-﻿use master;
-
-DROP DATABASE IF EXISTS bankAccount
+﻿DROP DATABASE IF EXISTS bankAccount
 
 CREATE DATABASE bankAccount
 GO
 use bankAccount;
 
-CREATE TABLE [user](userId Int IDENTITY(1,1), userName NVARCHAR(32) CHECK(LEN(userName) >= 8), password NVARCHAR(32) CHECK(LEN([password]) >= 8), PRIMARY KEY(userId) )
+CREATE TABLE [user](userId INT IDENTITY(1,1), userName NVARCHAR(32) CHECK(LEN(userName) >= 8), [password] NVARCHAR(32) CHECK(LEN([password]) >= 8), PRIMARY KEY(userId) )
 GO
+
+CREATE TABLE bankAccount(bankAccountId INT IDENTITY(1,1), balance DECIMAL DEFAULT(0), userId INT ,PRIMARY KEY(bankAccountId))
+GO
+
+ALTER TABLE bankAccount ADD CONSTRAINT FK_UserId FOREIGN KEY (userId) REFERENCES [user](userId)
+
+CREATE TABLE loan(loanId INT IDENTITY(1,1), paymentTime INT NOT NULL, debt DECIMAL DEFAULT(0), userId INT, PRIMARY KEY(loanId))
+GO
+
+ALTER TABLE loan ADD CONSTRAINT	FK_UserId_Loan FOREIGN KEY (userId) REFERENCES [user](userId)
