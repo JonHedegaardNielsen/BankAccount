f507b79b4f50a59489b3d276c632087242c56c68
jon <jon230706@gmail.com>
2024-10-23 14:56:45 +0200

    cleanup

--- Pages/Bank/CreateLoanPage.axaml.cs
+++ Pages/Bank/CreateLoanPage.axaml.cs
@@ -1,58 +1,12 @@
 using Avalonia;
 using Avalonia.Controls;
-using Avalonia.Interactivity;
-using Avalonia.Markup.Xaml;
-using System.Collections.Generic;
-using System.Linq;
-
 
 namespace BankAccount;
 public partial class CreateLoanPage : UserControl
 {
-
-	Loan? SelectedLoanType;
-
-	List<Loan> LoanTypes = new List<Loan>()
-	{
-		new("House Loan",PaymentTypes.Monthly , 10000000 , 1.5m, 5000),
-		new("Car Loan", PaymentTypes.Quarterly, 100000, 3, 2000),
-		new("Quick Loan", PaymentTypes.Monthly , 5000, 20, 1000)
-	};
-	List<BankAccount> BankAccounts;
-
 	public CreateLoanPage()
 	{
 		InitializeComponent();
 		DataContext = new CreateLoanPageViewModel(MainContent.Content);
 	}
-
-	private void PageLoaded(object sender, RoutedEventArgs e)
-	{
-		comboBoxLoanTypes.ItemsSource = LoanTypes;
-		comboBoxBankAccount.ItemsSource = BankAccounts;
-	}
-
-	private void SelectLoan(object sender, SelectionChangedEventArgs e)
-	{
-		ComboBox comboBox = (ComboBox)sender;
-		SelectedLoanType = (Loan?)comboBox.SelectedItem;
-		DataContext = SelectedLoanType;
-	}
-
-	private void CreateLoan(object sender, RoutedEventArgs e)
-	{
-		if (SelectedLoanType != null && comboBoxBankAccount.SelectedItem != null)
-		{
-			BankAccount bankAccount = (BankAccount)comboBoxBankAccount.SelectedItem;
-			SelectedLoanType.ChangePayDate();
-			LoanDatabase.Instance.Insert(SelectedLoanType, BankUser.CurrentUser.Id, bankAccount.Id);
-			bankAccount.Balance += SelectedLoanType.InitialValue;
-			BankAccountDatabase.Instance.UpdateBalance(bankAccount.Id, bankAccount.Balance);
-			BankUser.CurrentUser.Loans.Add(SelectedLoanType);
-			SelectedLoanType = null;
-			comboBoxLoanTypes.Clear();
-			comboBoxLoanTypes.ItemsSource = LoanTypes;
-		}
-	}
-
 }
